4e434a64469a706a6b9cbe29e28d1e09b8371a80
LeonSedlanic <voidx@net.hr>
2024-08-21 20:43:25 +0200

    Modify user class with daily values

--- app/src/main/java/com/example/calonote/model/User.kt
+++ app/src/main/java/com/example/calonote/model/User.kt
@@ -5,9 +5,14 @@ data class User(
     val email: String = "",
     val username: String = "",
     val isEmailVerified: Boolean = false,
-    val height: Double = 0.0, // in meters
-    val weight: Double = 0.0, // in kilograms
+    val height: Double = 0.0,
+    val weight: Double = 0.0,
     val age: Int = 0,
-    val desiredWeight: Double = 0.0, // in kilograms
-    val activityLevel: Int = 0 // 1 to 5
+    val desiredWeight: Double = 0.0,
+    val activityLevel: Int = 0,
+    val dailyCalories: Float = 0f,
+    val dailyProtein: Float = 0f,
+    val dailyFat: Float = 0f,
+    val dailyCarbs: Float = 0f,
+    val dailyVitamins: Float = 0f
 )
